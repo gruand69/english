01ba1f2135155f46fa6da8193fc689bcddcbd023
gruand69 <gruand69@yandex.ru>
2026-03-22 16:55:39 +0300

    ex1

--- ex1.py
+++ ex1.py
@@ -1,9 +1,8 @@
+import random
 import os
 import json
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QFont
-from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
-from check_phrase import CheckWindow
 
 
 class MyWindow(QtWidgets.QDialog):
@@ -11,6 +10,15 @@ class MyWindow(QtWidgets.QDialog):
         self.lst = lst
         self.i = 0
         QtWidgets.QWidget.__init__(self, parent)
+
+        self.labelTask = QtWidgets.QLabel(
+            'Choose the most appropriate short answer for each question.')
+        self.labelTask.setWordWrap(True)
+        self.labelTask.setAlignment(
+            QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
+        self.labelTask.setFont(QFont('Times', 14))
+        self.labelTask.setStyleSheet(
+            "border: 1px dashed black; border-radius: 10px;")
 
         self.labelQst = QtWidgets.QLabel(self.lst[self.i]['question'])
         self.labelQst.setWordWrap(True)
@@ -20,82 +28,88 @@ class MyWindow(QtWidgets.QDialog):
         self.labelQst.setStyleSheet(
             "border: 1px dashed black; border-radius: 10px;")
 
+        self.btnCheck = QtWidgets.QPushButton("&Check")
+        self.btnNext = QtWidgets.QPushButton("&Next >>")
+        self.btnBack = QtWidgets.QPushButton("<< &Back")
 
-        # self.btnBack = QtWidgets.QPushButton("<< &Back")
-        # self.btnForw = QtWidgets.QPushButton("&Forward >>")
-        # self.btnCheck = QtWidgets.QPushButton("&Prase")
-        # self.btnAudio = QtWidgets.QPushButton("&Audio")
-        # self.btnQuit = QtWidgets.QPushButton("&Close the window")
+        self.radBtn = QtWidgets.QVBoxLayout()
+        self.radBtn_group = QtWidgets.QButtonGroup()
+        self.btns = []
+        for resp in self.lst[self.i]['response']:
+            btn = QtWidgets.QRadioButton(resp)
+            self.radBtn.addWidget(btn)
+            self.radBtn_group.addButton(btn)
+            self.btns.append(btn)
+        self.btns[random.randint(
+            0, len(self.lst[self.i]['response'])-1)].setChecked(True)
 
-        self.vboxLabel = QtWidgets.QVBoxLayout()
-        self.vboxLabel.addWidget(self.labelRus)
-        self.vboxLabel.addWidget(self.labelEng)
+        self.hboxQstResp = QtWidgets.QHBoxLayout()
+        self.hboxQstResp.addWidget(self.labelQst)
+        self.hboxQstResp.addLayout(self.radBtn)
 
-        self.vboxNavBtn = QtWidgets.QHBoxLayout()
-        self.vboxNavBtn.addWidget(self.btnBack)
-        self.vboxNavBtn.addWidget(self.btnForw)
+        self.hboxNavBtn = QtWidgets.QHBoxLayout()
+        self.hboxNavBtn.addWidget(self.btnCheck)
+        self.hboxNavBtn.addWidget(self.btnBack)
+        self.hboxNavBtn.addWidget(self.btnNext)
 
         self.vboxVert = QtWidgets.QVBoxLayout()
-        self.vboxVert.addLayout(self.vboxLabel)
-        self.vboxVert.addLayout(self.vboxNavBtn)
+        self.vboxVert.addWidget(self.labelTask)
+        self.vboxVert.addLayout(self.hboxQstResp)
+        self.vboxVert.addLayout(self.hboxNavBtn)
 
-        self.vboxManBtn = QtWidgets.QVBoxLayout()
-        self.vboxManBtn.addWidget(self.btnCheck)
-        self.vboxManBtn.addWidget(self.btnAudio)
-        self.vboxManBtn.addWidget(self.btnQuit)
-
-        self.MainLayout = QtWidgets.QHBoxLayout()
-        self.MainLayout.addLayout(self.vboxVert)
-        self.MainLayout.addLayout(self.vboxManBtn)
-
-        self.setLayout(self.MainLayout)
+        self.setLayout(self.vboxVert)
         self.setGeometry(400, 200, 600, 300)
 
-        self.btnQuit.clicked.connect(QtWidgets.qApp.quit)
-        self.btnBack.clicked.connect(self.on_btnBack_clicked)
-        self.btnForw.clicked.connect(self.on_btnForw_clicked)
         self.btnCheck.clicked.connect(self.on_btnCheck_clicked)
-        self.btnAudio.clicked.connect(self.on_btnAudio_clicked)
-
-    def on_btnAudio_clicked(self):
-        dirname = os.path.dirname(__file__)
-        mc = QMediaContent(QtCore.QUrl.fromLocalFile(
-            os.path.join(os.path.join(dirname,
-                                      f'media\dialog1\{self.i+1}.mp3'))))
-        self.mp.setMedia(mc)
-        self.mp.stop()
-        self.mp.setVolume(50)
-        self.mp.setPosition(0)
-        self.mp.play()
+        self.btnNext.clicked.connect(self.on_btnNext_clicked)
+        self.btnBack.clicked.connect(self.on_btnBack_clicked)
 
     def on_btnCheck_clicked(self):
-        dialog = CheckWindow(self.lst[self.i]['question'],
-                             self.lst[self.i]['response'])
-        dialog.exec_()
+        n = self.radBtn_group.checkedButton().text()
+        if n == self.lst[self.i]['response'][self.lst[self.i]['correct']-1]:
+            QtWidgets.QMessageBox.information(self, 'Message', 'Excellent!')
+        else:
+            QtWidgets.QMessageBox.information(
+                self, 'Message', 'Not exactly, Try again!')
 
-    def on_btnForw_clicked(self):
+    def cleanData(self):
+        self.labelQst.setText(self.lst[self.i]['question'])
+        while self.radBtn.count():
+            item = self.radBtn.takeAt(0)  # Берем первый элемент
+            widget = item.widget()   # Получаем виджет
+            if widget is not None:
+                widget.deleteLater()  # Удаляем виджет
+        for i in self.radBtn_group.buttons():
+            i.deleteLater()
+
+        self.btns.clear()
+        for resp in self.lst[self.i]['response']:
+            btn = QtWidgets.QRadioButton(resp)
+            self.radBtn.addWidget(btn)
+            self.radBtn_group.addButton(btn)
+            self.btns.append(btn)
+        self.btns[random.randint(
+            0, len(self.lst[self.i]['response'])-1)].setChecked(True)
+
+    def on_btnNext_clicked(self):
         if self.i < len(self.lst) - 1:
             self.i += 1
-            self.labelRus.setText(self.lst[self.i]['question'])
-            self.labelEng.setText(self.lst[self.i]['response'])
+            self.cleanData()
 
     def on_btnBack_clicked(self):
         if self.i > 0:
             self.i -= 1
-            self.labelRus.setText(self.lst[self.i]['question'])
-            self.labelEng.setText(self.lst[self.i]['response'])
+            self.cleanData()
 
 
 if __name__ == "__main__":
     dirname = os.path.dirname(__file__)
     with open(os.path.join(dirname, 'json/ex1.json'), encoding='utf-8') as f:
         data = json.load(f)
-    # print(data['test'][1])
 
     import sys
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow(data['test'])
-    window.setWindowTitle("OOP-style creating the window")
-    # window.resize(300, 70)
+    window.setWindowTitle("Ex1")
     window.show()
     sys.exit(app.exec_())
